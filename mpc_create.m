d4146da436b932cd3ee640f8b94e1ae3837f8bb2
axelnugroho <70876269+axelnugroho@users.noreply.github.com>
2020-10-06 18:20:58 +0700

    Update mpc_create.m

--- mpc_create.m
+++ mpc_create.m
@@ -1,63 +1,36 @@
-function [Phi_Phi, Phi_F, Phi_R, eyeNc] = mpc_model(num_c, den_c, Delta_t, Nc, Np, display)
-[Ap,Bp,Cp,Dp]=tf2ss(num_c,den_c);
-Delta_t=0.01;
-[Ad,Bd,Cd,Dd]=c2dm(Ap,Bp,Cp,Dp,Delta_t);
-
-[m1,n1]=size(Cd);
-[n1,n_in]=size(Bd);
-A_e=eye(n1+m1,n1+m1);
-A_e(1:n1,1:n1)=Ad;
-A_e(n1+1:n1+m1,1:n1)=Cd*Ad;
-B_e=zeros(n1+m1,n_in);
-B_e(1:n1,:)=Bd;
-B_e(n1+1:n1+m1,:)=Cd*Bd;
-C_e=zeros(m1,n1+m1);
-C_e(:,n1+1:n1+m1)=eye(m1,m1);
-
-n=n1+m1;
-h(1,:)=C_e;
-F(1,:)=C_e*A_e;
-for kk=2:Np
-    h(kk,:)=h(kk-1,:)*A_e;
-    F(kk,:)= F(kk-1,:)*A_e;
-end
-v=h*B_e;
-Phi=zeros(Np,Nc); % declare the dimension of Phi
-Phi(:,1)=v; % first column of Phi
-for i=2:Nc
-    Phi(:,i)=[zeros(i-1,1);v(1:Np-i+1,1)]; %Toeplitz matrix
-end
-BarRs=ones(Np,1);
-Phi_Phi= Phi'*Phi;
-Phi_F= Phi'*F;
-Phi_R=Phi'*BarRs;
-eyeNc = eye(Nc,Nc);
-
-if display=='YES'
-[n,n_in]=size(B_e);
-xm=zeros(n1,1);
-Xf=zeros(n,1);
-N_sim=100;
-r=ones(N_sim,1);
-u=0;
-y=0;
-for kk=1:N_sim;
-DeltaU=inv(Phi_Phi+0.1*eye(Nc,Nc))*(Phi_R*r(kk)-Phi_F*Xf);
-deltau=DeltaU(1,1);
-u=u+deltau;
-u1(kk)=u;
-y1(kk)=y;
-xm_old=xm;
-xm=Ad.*xm+Bd.*u;
-y=Cd*xm+Dd*u;
-Xf=[xm-xm_old;y];
-end
-
-for i = 0:99
-    k(i+1) = i * Delta_t;
-end
-figure
-plot(k,y1)
-grid;
-end
-end
+clear;clc;
+%% System Model
+disp('=== System Model ===');
+num_str = input('Numerator     = ','s');
+den_str = input('Denominator   = ','s');
+Ts      = input('Time sampling = ');
+num = str2num(num_str);
+den = str2num(den_str);
+%% MPC
+disp(' ');
+disp('=== MPC MODEL ===');
+disp('Nc is horizon control');
+disp('Np is horizon predictive');
+disp('k is gain system');
+Nc = input('Nc = ');
+Np = input('Np = ');
+k  = input('k  = ');
+disp('=== DISPLAY ===');
+disp('only can display SISO system');
+display = input('YES/NO : ','s');
+[Phi_Phi, Phi_F, Phi_R, eye_Nc] = mpc_model(num, den, Ts, Nc, Np,display);
+Keye_Nc = k*eye_Nc;
+disp(' ');
+disp('model MPC = ((Phi_Phi+k*eye(Nc,Nc))*(Phi_R*r(i)-Phi_F*Xf))^(-1)');
+disp('r(i): input system');
+disp('k   : gain system');
+disp('Xf  : delta error sensor');
+disp(' ');
+disp('Phi_Phi = ');
+disp(Phi_Phi);
+disp('Phi_F = ');
+disp(Phi_F);
+disp('Phi_R = ');
+disp(Phi_R);
+disp('k*eye(Nc,Nc) = ');
+disp(Keye_Nc);
